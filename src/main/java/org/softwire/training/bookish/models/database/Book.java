61365d9f7e26278189a8899bb0e5ed2718d7e479
Alexandru Catarau <alexctrx@gmail.com>
2019-11-04 15:58:34 +0000

    Connected the web app to database

--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -2,4 +2,51 @@ package org.softwire.training.bookish.models.database;
 
 public class Book {
 
+    private String id;
+    private String isbn;
+    private String title;
+
+
+    public Book() {
+    }
+
+    public Book(String id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/softwire/training/bookish/services/DatabaseService.java
+++ src/main/java/org/softwire/training/bookish/services/DatabaseService.java
@@ -7,7 +7,7 @@ public abstract class DatabaseService {
     private final String hostname = "localhost";
     private final String database = System.getenv("DB_NAME");
     private final String user = System.getenv("DB_USER");
-    private final String password = System.getenv("DB_PASS");
+    private final String password = System.getenv("DB_PASSWORD");
     private final String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
     protected final Jdbi jdbi = Jdbi.create(connectionString);
